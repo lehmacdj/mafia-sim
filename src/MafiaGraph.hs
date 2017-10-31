ae5a273e02441ec661205cc13fdf8dbc78f30c91
Devin Lehmacher <lehmacdj@gmail.com>
2017-10-30 21:06:15 -0400

    use a list of edges as mid section edges

--- src/MafiaGraph.hs
+++ src/MafiaGraph.hs
@@ -41,10 +41,10 @@ instance Eq a => Semigroup (Segment a) where
     Just z <- [compose x y]
     pure z
 
-data Action a = A (Segment a) (Segment a) (Segment a)
+data Action a = A (Segment a) [Rel a] (Segment a)
 
 instance Eq a => Semigroup (Action a) where
-  A s1 s2 s3 <> A t1 t2 t3 = A (s1 <> t1) (s2 <> t2) (s3 <> t3)
+  A s1 s2 s3 <> A t1 t2 t3 = A (s1 <> t1) (s2 ++ t2) (s3 <> t3)
 
 data Event a = Visiting a a
              | Killing a a
@@ -73,18 +73,16 @@ identity :: [a] -> Segment a
 identity xs = S [ R x Trans x | x <- xs ]
 
 block :: Eq a => [a] -> a -> a -> Action a
-block ps p q = A r a s where
+block ps p q = A r [R p Visit q] s where
   r = identity (delete q ps)
-  a = S [R p Visit q] <> identity ps
   s = identity ps
 
 swap :: Eq a => [a] -> a -> a -> a -> Action a
-swap ps p q r = A (identity ps) a s where
-  a = S [R p Visit q, R p Visit r] <> identity ps
+swap ps p q r = A (identity ps) [R p Visit q, R p Visit r] s where
   s = S [R q Trans r, R r Trans q] <> identity (ps \\ [q, r])
 
 simple :: Eq a => [a] -> [Rel a] -> Action a
-simple xs rs = A (identity xs) (S rs <> identity xs) (identity xs)
+simple xs rs = A (identity xs) rs (identity xs)
 
 interpret :: Eq a => [a] -> [Event a] -> Action a
 interpret xs (Visiting p q : ts) = simple xs [R p Visit q] <> interpret xs ts
@@ -96,3 +94,5 @@ interpret xs (Swapping p q r : ts) = swap xs p q r <> interpret xs ts
 interpret xs (Oiling p q : ts) = simple xs [R p Oil q, R p Visit q] <> interpret xs ts
 interpret xs (LightUp p : ts) = simple xs (map (R p Light) xs) <> interpret xs ts
 interpret xs (PerformRitual p : ts) = simple xs (map (R p Save) xs) <> interpret xs ts
+interpret xs (Distract p q : ts) = undefined
+interpret xs (UseStrength p q : ts) = undefined
