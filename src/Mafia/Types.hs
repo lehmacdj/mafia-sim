76f2da9e80afa3d28d194715db0bee55fe44c217
Devin Lehmacher <lehmacdj@gmail.com>
2017-11-01 17:04:47 -0400

    document types better

--- src/Mafia/Types.hs
+++ src/Mafia/Types.hs
@@ -19,8 +19,6 @@ data Ability a = ARoleblock a -- e.g. bootlegger
                | ASimple SimpleAbility a -- applies several effects
                | ALight -- e.g. arsonist
                | ARitual -- e.g. virgin
-               -- the position of strength in list determines if it goes through
-               -- swaps or not. bodyguard is not handled correctly
                | AStrength a -- e.g. strongman
                | ASwap a a -- e.g. illusionist
                | AGuard a -- e.g. bodyguard
@@ -31,19 +29,31 @@ data Event a = E a (Ability a)
 
 type Trace a = [Event a]
 
-data Effect = Kill
-            | Oil
-            | Conceal
-            | Silence
-            | Check
-            | Visit
-            | Light
-            | Track
-            | Watch
-            | GetRole
-            | Oracle
+data Effect = Kill -- kill a player
+            | Oil -- add oil to a player
+            | Conceal -- prevent a kill from being announced
+            | Silence -- prevent a player from talking
+            | Check -- sheriff check someone
+            | Visit -- visited for tracker/watcher
+            | Light -- use up oil on a player or prevent kills by same player
+            | Track -- track someone
+            | Watch -- watch someone
+            | GetRole -- learn the role of a player
+            | Oracle -- reveal the role of player when cause of effect dies
             deriving (Eq, Show)
 
+-- the type we should get after one night is a list of
+data Action a = Action
+  { src :: a
+  , eff :: Effect
+  , dest :: a
+  }
+  deriving (Show, Eq)
+
+-- the result of executing a trace
+type Result a = [Action a]
+
+-- attributes for global state, for multiple games
 data Attr = Anti Effect
           | Town
           | Mafia
