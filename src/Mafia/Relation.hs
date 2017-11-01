fe5514d09ae8cd25947cd9e3b6690a6540627a8c
Devin Lehmacher <lehmacdj@gmail.com>
2017-11-01 17:32:19 -0400

    implement an ordering + strongman

--- src/Mafia/Relation.hs
+++ src/Mafia/Relation.hs
@@ -5,13 +5,14 @@ module Mafia.Relation where
 import Mafia.Types
 
 import Control.Arrow
-import Data.List ((\\), intersect)
+import Data.List ((\\), intersect, sort, elemIndex)
 import Data.Semigroup
 import Data.Map (Map)
 import qualified Data.Map as Map
 
 data EAux e = Effect e
             | CanMutate -- is player allowed to provide subset of identity
+            | StrongKill
             deriving (Eq, Show)
 
 data EType = EAux (EAux CondEffect)
@@ -25,6 +26,7 @@ data CondEffect = When Effect Effect
 concreteEffects :: [EAux Effect]
 concreteEffects =
   [ Effect Kill
+  , StrongKill
   , Effect Oil
   , Effect Conceal
   , Effect Silence
@@ -50,6 +52,8 @@ comp (Effect (Always e)) = Effect e
 comp CanMutate = CanMutate
 
 composeEdge :: Eq a => Edge a -> Edge a -> Maybe (Edge a)
+composeEdge (Edge e@(EAux StrongKill) p q) _ = Just $ Edge e p q
+composeEdge _ (Edge e@(EAux StrongKill) p q) = Just $ Edge e p q
 composeEdge (Edge e p q) (Edge e' q' r)
   | q == q' = case (e, e') of
     (Compose es, Compose es') -> Just $ Edge (Compose $ es `intersect` es') p r
@@ -58,30 +62,30 @@ composeEdge (Edge e p q) (Edge e' q' r)
     _ -> Nothing
   | otherwise = Nothing
 
-newtype Action a = A [Edge a]
+newtype Rel a = R [Edge a]
   deriving (Eq, Show, Functor)
 
--- horizontal composition of Actions is a semigroup
-instance Eq a => Semigroup (Action a) where
-  A xs <> A ys = A [ z | x <- xs, y <- ys, Just z <- [composeEdge x y] ]
+-- horizontal composition of Rel is a semigroup
+instance Eq a => Semigroup (Rel a) where
+  R xs <> R ys = R [ z | x <- xs, y <- ys, Just z <- [composeEdge x y] ]
 
 -- there is also however a notion of vertical composition that just adds
 -- stuff to the relation
 infixr 8 <@>
-(<@>) :: Eq a => Action a -> Action a -> Action a
-A xs <@> A ys = A $ xs ++ ys
+(<@>) :: Eq a => Rel a -> Rel a -> Rel a
+R xs <@> R ys = R $ xs ++ ys
 
-composeEdges :: [EAux Effect] -> [a] -> Action a
-composeEdges ts xs = A [ Edge (Compose ts) x x | x <- xs ]
+composeEdges :: [EAux Effect] -> [a] -> Rel a
+composeEdges ts xs = R [ Edge (Compose ts) x x | x <- xs ]
 
-identity :: [a] -> Action a
+identity :: [a] -> Rel a
 identity = composeEdges concreteEffects
 
-initial :: Eq a => [a] -> Action a
-initial xs = identity xs <@> A [ Edge (EAux CanMutate) x x | x <- xs ]
+initial :: Eq a => [a] -> Rel a
+initial xs = identity xs <@> R [ Edge (EAux CanMutate) x x | x <- xs ]
 
-simple :: Eq a => [a] -> [Edge a] -> Action a
-simple xs es = identity xs <@> A es
+simple :: Eq a => [a] -> [Edge a] -> Rel a
+simple xs es = identity xs <@> R es
 
 edge :: Effect -> a -> a -> Edge a
 edge = edge' Always
@@ -89,13 +93,13 @@ edge = edge' Always
 edge' :: (Effect -> CondEffect) -> Effect -> a -> a -> Edge a
 edge' c e = Edge (EAux $ Effect $ c e)
 
-hasEffect :: Eq a => EAux CondEffect -> a -> Action a -> Bool
-hasEffect e x (A es) = any (\z -> case z of
+hasEffect :: Eq a => EAux CondEffect -> a -> Rel a -> Bool
+hasEffect e x (R es) = any (\z -> case z of
   Edge (EAux e') _ x' -> e == e' && x == x'
   _ -> False) es
 
-flattenEdges :: Action a -> [(a, CondEffect, a)]
-flattenEdges (A rs) = [ (p, e, q) | Edge (EAux (Effect e)) p q <- rs ]
+flattenEdges :: Rel a -> [(a, CondEffect, a)]
+flattenEdges (R rs) = [ (p, e, q) | Edge (EAux (Effect e)) p q <- rs ]
 
 rcond :: [(a, CondEffect, a)] -> [(a, Effect, a)]
 rcond xs = [ (p, e, q) | (p, Always e, q) <- xs ]
@@ -109,16 +113,19 @@ eliminateConditions xs = go xs where
     | otherwise = go ys
   go ((p, Always e, q):ys) = (p, e, q) : go ys
 
-reduceEdges :: Eq a => Action a -> [(a, Effect, a)]
+reduceEdges :: Eq a => Rel a -> [(a, Effect, a)]
 reduceEdges = eliminateConditions . flattenEdges
 
-getAffectedByMap :: Ord a => [a] -> Action a -> Map a [(a, Effect)]
+getAffectedByMap :: Ord a => [a] -> Rel a -> Map a [(a, Effect)]
 getAffectedByMap xs a = Map.fromList l where
   l = map (\q -> (q, [(p, e) | (p, e, q') <- reduceEdges a, q == q'])) xs
 
-getEffectingMap :: Ord a => [a] -> Action a -> Map a [(a, Effect)]
+getEffectingMap :: Ord a => [a] -> Rel a -> Map a [(a, Effect)]
 getEffectingMap xs a = Map.fromList l where
   l = map (\p -> (p, [(q, e) | (p', e, q) <- reduceEdges a, p == p'])) xs
+
+getResult :: Ord a => Rel a -> Result a
+getResult = map (\(p, e, q) -> Action p e q) . reduceEdges
 
 denoteSA :: SimpleAbility -> [CondEffect]
 denoteSA AKill = [Always Kill, When Kill Visit]
@@ -135,27 +142,27 @@ denoteSA AGravedig = [Always Kill, When Kill GetRole, When Kill Conceal, When Ki
 edgesFromSA :: SimpleAbility -> a -> a -> [Edge a]
 edgesFromSA a x y = map (\e -> Edge (EAux $ Effect e) x y) (denoteSA a)
 
-canMutate :: Eq a => a -> Action a -> Bool
+canMutate :: Eq a => a -> Rel a -> Bool
 canMutate = hasEffect CanMutate
 
-visit :: Eq a => [a] -> a -> a -> Action a
+visit :: Eq a => [a] -> a -> a -> Rel a
 visit xs x y = simple xs [edge Visit x y]
 
-swapEdge :: a -> a -> Action a
-swapEdge x y = A [Edge (Compose concreteEffects) x y]
+swapEdge :: a -> a -> Rel a
+swapEdge x y = R [Edge (Compose concreteEffects) x y]
 
 noKill :: [EAux Effect]
 noKill = concreteEffects \\ [Effect Kill]
 
 -- a folding function for a trace
-augment :: Eq a => [a] -> Event a -> Action a -> Action a
+augment :: Eq a => [a] -> Event a -> Rel a -> Rel a
 augment xs (E x (ASimple a y)) ac = ac <> simple xs (edgesFromSA a x y)
 augment xs (E x (ASave y)) ac
-  | canMutate x ac = ac <> save <@> A [edge Visit x y] where
+  | canMutate x ac = ac <> save <@> R [edge Visit x y] where
     save = composeEdges noKill [y] <@> identity (xs \\ [y])
 -- roleblocking doesn't care if it is allowed to mutate
 augment xs (E x (ARoleblock y)) ac = roleblock <> ac <> visit xs x y where
-    roleblock = identity (xs \\ [y])
+  roleblock = identity (xs \\ [y])
 augment xs (E x (ADistract y)) ac
   | canMutate x ac = distract <> ac <> visit xs x y where
     distract = identity (xs \\ [y]) <@> composeEdges noKill [y]
@@ -163,22 +170,72 @@ augment xs (E x ALight) ac = ac <> simple xs (f =<< xs) where
   f y = [edge' (When Kill) Light x y, edge Kill x y]
 augment xs (E x ARitual) ac | canMutate x ac = ac <> composeEdges noKill xs
 augment xs (E x (ASwap y z)) ac
-  | canMutate x ac = ac <> swap <@> A [edge Visit x y, edge Visit x z] where
+  | canMutate x ac = ac <> swap <@> R [edge Visit x y, edge Visit x z] where
     swap = identity (xs \\ [y, z]) <@> swapEdge y z <@> swapEdge z y
 augment xs (E x (AGuard y)) ac
-  | canMutate x ac = ac <> guard' <@> A [edge Visit x y] where
+  | canMutate x ac = ac <> guard' <@> R [edge Visit x y] where
     guard' = identity (xs \\ [y]) <@> swapEdge y x
-augment xs (E x (AStrength y)) ac = (ac <> simple xs killEdges) <@> A killEdges where
-  killEdges = [edge Kill x y, edge' (When Kill) Visit x y]
+augment xs (E x (AStrength y)) ac = (simple xs k <> ac) <@> R sk where
+  sk = [Edge (EAux StrongKill) x y]
+  k = [edge Kill x y, edge' (When Kill) Visit x y]
 augment _ _ ac = ac
 
-data Result a = Result
-  { affectedByMap :: Map a [(a, Effect)]
-  , effectingMap :: Map a [(a, Effect)]
-  }
-  deriving (Eq, Show)
+newtype TraceOrd a = TO { unTO :: Event a }
+  deriving (Show)
+
+-- whnf equality
+instance Eq (TraceOrd a) where
+  TO (E _ (ARoleblock _)) == TO (E _ (ARoleblock _)) = True
+  TO (E _ (ADistract _)) == TO (E _ (ADistract _)) = True
+  TO (E _ (ASave _)) == TO (E _ (ASave _)) = True
+  TO (E _ (ASimple sa _)) == TO (E _ (ASimple sa' _)) = sa == sa'
+  TO (E _ ALight) == TO (E _ ALight) = True
+  TO (E _ ARitual) == TO (E _ ARitual) = True
+  TO (E _ (AStrength _)) == TO (E _ (AStrength _)) = True
+  TO (E _ (ASwap _ _)) == TO (E _ (ASwap _ _)) = True
+  TO (E _ (AGuard _)) == TO (E _ (AGuard _)) = True
+  _ == _ = False
+
+-- we never evaluate these beyond WHNF so undefined is fine here
+actionOrder :: [TraceOrd a]
+actionOrder = map (TO . E undefined . ($ undefined)) abilities where
+  abilities =
+    [ ASimple AKill
+    , ASimple AOil
+    , ASimple ATrack
+    , ASimple AOracle
+    , ASimple AWatch
+    , ASimple ACheck
+    , ASimple AGetRole
+    , ASimple ASilence
+    , ASimple AVisit
+    , ASimple AGravedig
+    , const ALight
+    , const ARitual
+    -- saves are affected by swaps thus before them
+    , ASave
+    -- swaps happen after all of the concrete roles
+    , ASwap undefined
+    -- strength goes after swap so it is unaffected by it
+    , AStrength
+    -- guard does affect strength so it occurs next
+    , AGuard
+    -- strength is post save to go through saves
+    -- roleblocks occur in inverse priority order composing on left
+    , ADistract
+    , ARoleblock
+    ]
+
+instance Ord (TraceOrd a) where
+  compare x y = case (x `elemIndex` actionOrder, y `elemIndex` actionOrder) of
+    (Just x', Just y')
+      | x' > y' -> GT
+      | x' == y' -> EQ
+      | otherwise -> LT
+    -- if both aren't Just, something went wrong
+    _ -> undefined
 
 runTrace :: Ord a => [a] -> Trace a -> Result a
-runTrace xs = uncurry Result
-  . (getAffectedByMap xs &&& getEffectingMap xs)
-  . foldr (augment xs) (initial xs)
+runTrace xs = getResult
+  . foldl (flip $ augment xs) (initial xs)
+  . map unTO . sort . map TO
